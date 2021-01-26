7996ff528bc280a2c2b3ce43aad2705e29fc7fe9
Oleg Korneev <olvikorneev@gmail.com>
2021-01-26 15:02:59 +0300

    Swiper in Gallery section doing

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+
+  // slidesPerColumn: 2,
+
+  // Optional parameters
+  // direction: 'vertical',
+  // loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
+
+
+const headerSelect = () => {
+  const elements = document.querySelectorAll('.header-select');
+  elements.forEach(el => {
+    const choices = new Choices(el, {
+      searchEnabled: false,
+      itemSelectText: '',
+      sorter: function (a, b) {
+      },
+      position: 'bottom',
+    });
+  });
+};
+
+headerSelect ();
+
